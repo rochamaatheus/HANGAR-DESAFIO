0d8a36b3d0551d1abda67ec1fa00c2f5eca0a4ea
rochamaatheus <matheussilveirarocha.sc@gmail.com>
2024-04-01 20:45:02 -0300

    Modificando o layout da página

--- resposta_1.php
+++ resposta_1.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Média de Pedidos</title>
     <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/tabela.css">
     <style>
         .red { color: red; }
         .green { color: green; }
@@ -13,6 +14,8 @@
 </head>
 <body>
     <?php
+      include("./include/header.php");
+
       ini_set('display_errors', '0');
       /** @var mysqli $db_connection */
       include('./conexao.php');
@@ -33,40 +36,44 @@
         $result = $stmt->get_result();
     ?>
 
-    <table>
-        <thead>
-            <tr>
-                <th>Data</th>
-                <th>Média</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php while ($row = mysqli_fetch_assoc($result)): ?>
-            <?php
-                $color = '';
-                if ($row['average'] < 3000) {
-                 $color = 'red';
-                } elseif ($row['average'] > 3000) {
-                 $color = 'green';
-                } else {
-                 $color = 'gray';
-                }
-            ?>
-            <tr class="<?= $color; ?>">
-                <td><?= htmlspecialchars($row['date']); ?></td>
-                <td><?= htmlspecialchars($row['average']); ?></td>
-            </tr>
-            <?php endwhile; } catch (Exception $e) {
-                $logMessage = date('Y-m-d H:i:s') . ' - Erro: ' . $e->getMessage() . "\n";
-                file_put_contents('error_log_resposta_1.txt', $logMessage, FILE_APPEND);
-                echo "<p>Ocorreu um erro. Por favor, tente novamente mais tarde.</p>";
-            } finally {
-                if ($stmt !== null) {
-                    $stmt->close();
-                }
-                $db_connection->close();
-            } ?>
-        </tbody>
-    </table>
+    <main class="container">
+        <table>
+            <thead>
+                <tr>
+                    <th>Data</th>
+                    <th>Média</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php while ($row = mysqli_fetch_assoc($result)): ?>
+                <?php
+                    $color = '';
+                    if ($row['average'] < 3000) {
+                     $color = 'red';
+                    } elseif ($row['average'] > 3000) {
+                     $color = 'green';
+                    } else {
+                     $color = 'gray';
+                    }
+                ?>
+                <tr class="<?= $color; ?>">
+                    <td><?= htmlspecialchars($row['date']); ?></td>
+                    <td><?= htmlspecialchars($row['average']); ?></td>
+                </tr>
+                <?php endwhile; } catch (Exception $e) {
+                    $logMessage = date('Y-m-d H:i:s') . ' - Erro: ' . $e->getMessage() . "\n";
+                    file_put_contents('error_log_resposta_1.txt', $logMessage, FILE_APPEND);
+                    echo "<p>Ocorreu um erro. Por favor, tente novamente mais tarde.</p>";
+                } finally {
+                    if ($stmt !== null) {
+                        $stmt->close();
+                    }
+                    $db_connection->close();
+                } ?>
+            </tbody>
+        </table>
+    </main>
+
+    <?php include("./include/footer.php"); ?>
 </body>
 </html>

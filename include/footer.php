45212843dff09a6711996f80ab71d29ac3af6a49
rochamaatheus <matheussilveirarocha.sc@gmail.com>
2024-04-01 20:36:14 -0300

    Criando Header e Footer include

---
+++ include/footer.php
@@ -0,0 +1,6 @@
+<footer>
+    <p>
+        Desenvolvido por &copy;<a href="https://github.com/rochamaatheus" target="_blank" rel="external">Matheus
+            Rocha</a>.
+    </p>
+</footer>
---
+++ include/header.php
@@ -0,0 +1,16 @@
+<header>
+    <nav>
+        <div class="logo">
+            <a href="index.php"><img src="./assets/logo.png" alt="Big Bang Theory Logo"></a>
+        </div>
+        <ul>
+            <li>Home</li>
+            <li>Teste</li>
+            <li>Teste</li>
+            <li>Teste</li>
+            <li>Teste</li>
+            <li>Teste</li>
+            <li>Teste</li>
+        </ul>
+    </nav>
+</header>

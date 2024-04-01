570d0386a81f399bce103121cef332f5ce610357
rochamaatheus <matheussilveirarocha.sc@gmail.com>
2024-04-01 19:36:42 -0300

    Esqueleto da página

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Big Bang Theory Inc</title>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Teste</li>
+                <li>Teste</li>
+                <li>Teste</li>
+                <li>Teste</li>
+                <li>Teste</li>
+                <li>Teste</li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <div class="container">
+            <div>
+                <div class="conteudo">
+                    <div>
+                        <h1>Sobre a Título</h1>
+                        <p>Descrição</p>
+                        <a href="cursos"><button class="btn-cursos">Cursos</button></a>
+                        <div>
+                            <h2>Avaliações</h2>
+                            <ul>
+                                <li>
+                                    <p>Nome da Avaliação</p>
+                                    <p>Descrição da Avaliação</p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <img src="./assets/sheldon.png" alt="Sheldon Cooper" />
+                </div>
+            </div>
+
+        </div>
+    </main>
+    <footer>
+        <p>Desenvolvido por &copy;<a href="https://github.com/rochamaatheus" target="_blank" rel="external">Matheus Rocha</a>.</p>
+    </footer>
+</body>
+
+</html>

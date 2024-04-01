08094d7c6cf51ab25183aebb6d93729eecdbe02c
rochamaatheus <matheussilveirarocha.sc@gmail.com>
2024-04-01 10:19:27 -0300

    Adiciona arquivo de conexão com o banco de dados

---
+++ conexao.php
@@ -0,0 +1,18 @@
+<?php 
+
+$db_server = 'localhost';
+$db_user = 'root';
+$db_password = '';
+$db_name = 'desafio-hangar';
+$db_connection = '';
+
+try {
+  $db_connection = @mysqli_connect($db_server, $db_user, $db_password, $db_name);
+  if (!$db_connection) {
+    throw new Exception('Could not connect to MySQL server.');
+  }
+  echo 'Connected to MySQL server.';
+} catch (Exception $e) {
+  echo $e->getMessage();
+}
+?>

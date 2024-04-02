c177859e7ef3968eefdb84b87d47d1a33cb208c1
rochamaatheus <matheussilveirarocha.sc@gmail.com>
2024-04-01 21:49:52 -0300

    Alteração layout página 6

--- paginas/resposta_6.php
+++ paginas/resposta_6.php
@@ -5,10 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Média de Pedidos</title>
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/tabela.css">
 </head>
 
 <body>
-    <form method="post">
+    <?php include("../include/header-paginas.php"); ?>
+
+    <form method="post" class="container">
         <button type="submit" name="submit">Executar Consulta</button>
     </form>
 
@@ -34,36 +38,39 @@
           $result = $stmt->get_result();
     ?>
 
-    <table>
-        <thead>
-            <tr>
-                <th>Ano</th>
-                <th>Mês</th>
-                <th>Total</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php while ($row = mysqli_fetch_assoc($result)): ?>
-            <tr>
-                <td><?= htmlspecialchars($row['Year']); ?></td>
-                <td><?= htmlspecialchars($row['Month']); ?></td>
-                <td><?= htmlspecialchars($row['Total']); ?></td>
-            </tr>
-            <?php endwhile; } catch (Exception $e) {
-                $logMessage = date('Y-m-d H:i:s') . ' - Erro: ' . $e->getMessage() . "\n";
-                file_put_contents('error_log_resposta_6.txt', $logMessage, FILE_APPEND);
-                echo "<p style=\"text-align: center;\">Ocorreu um erro. Por favor, tente novamente mais tarde.</p>";
-            } finally {
-                if ($stmt !== null) {
-                    $stmt->close();
-                }
-                $db_connection->close();
-            } ?>
-        </tbody>
-    </table>
+    <main class="container">
+        <table>
+            <thead>
+                <tr>
+                    <th>Ano</th>
+                    <th>Mês</th>
+                    <th>Total</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php while ($row = mysqli_fetch_assoc($result)): ?>
+                <tr>
+                    <td><?= htmlspecialchars($row['Year']); ?></td>
+                    <td><?= htmlspecialchars($row['Month']); ?></td>
+                    <td><?= htmlspecialchars($row['Total']); ?></td>
+                </tr>
+                <?php endwhile; } catch (Exception $e) {
+                    $logMessage = date('Y-m-d H:i:s') . ' - Erro: ' . $e->getMessage() . "\n";
+                    file_put_contents('error_log_resposta_6.txt', $logMessage, FILE_APPEND);
+                    echo "<p style=\"text-align: center;\">Ocorreu um erro. Por favor, tente novamente mais tarde.</p>";
+                } finally {
+                    if ($stmt !== null) {
+                        $stmt->close();
+                    }
+                    $db_connection->close();
+                } ?>
+            </tbody>
+        </table>
+    </main>
 
     <?php
       }
+      include("../include/footer.php");
     ?>
 </body>
 

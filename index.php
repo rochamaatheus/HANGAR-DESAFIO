a357fb900604b6a434baedb86434c0c059b54be0
rochamaatheus <matheussilveirarocha.sc@gmail.com>
2024-04-15 21:54:17 -0300

    Melhoria de responsividade

--- index.php
+++ index.php
@@ -2,53 +2,53 @@
 <html lang="pt-br">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Big Bang Theory Inc</title>
-    <link rel="stylesheet" href="./css/style.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Big Bang Theory Inc</title>
+  <link rel="stylesheet" href="./css/style.css">
 </head>
 
 <body>
-    <?php include('./include/header.php') ?>
+  <?php include('./include/header.php') ?>
 
-    <main>
-        <div class="container">
-            <div class="conteudo">
-                <div>
-                    <h1>Sobre a série <strong>Big Bang Theory</strong>.</h1>
-                    <p>"The Big Bang Theory" é uma série de comédia que foi ao ar de 2007 a 2019. A série segue a vida
-                        de quatro amigos intelectuais que são apaixonados por ciência e cultura geek, e sua vizinha
-                        Penny, que tem mais habilidades sociais. A interação entre esses personagens cria muitas das
-                        situações cômicas da série.</p>
-                    <a href="./paginas/resposta_1.php"><button class="btn-tabela">Comece a ver as tabelas</button></a>
-                    <div>
-                        <h2>Avaliações</h2>
-                        <ul>
-                            <li>
-                                <p><strong>Avaliação de João</strong></p>
-                                <p>"The Big Bang Theory é uma das minhas séries favoritas. A combinação de humor e
-                                    ciência é única e os personagens são inesquecíveis."</p>
-                            </li>
-                            <li>
-                                <p><strong>Avaliação de Maria</strong></p>
-                                <p>"Adoro como a série explora a dinâmica entre os personagens intelectuais e Penny. É
-                                    sempre divertido assistir."</p>
-                            </li>
-                            <li>
-                                <p><strong>Avaliação de Pedro</strong></p>
-                                <p>"A série faz um ótimo trabalho ao incorporar elementos da cultura geek em seu humor.
-                                    Sempre me pego rindo das piadas."</p>
-                            </li>
-                        </ul>
+  <main>
+    <div class="container">
+      <div class="conteudo">
+        <div>
+          <h1>Sobre a série <strong>Big Bang Theory</strong>.</h1>
+          <p>"The Big Bang Theory" é uma série de comédia que foi ao ar de 2007 a 2019. A série segue a vida
+            de quatro amigos intelectuais que são apaixonados por ciência e cultura geek, e sua vizinha
+            Penny, que tem mais habilidades sociais. A interação entre esses personagens cria muitas das
+            situações cômicas da série.</p>
+          <a href="./paginas/resposta_1.php"><button class="btn-tabela">Comece a ver as tabelas</button></a>
+          <div>
+            <h2>Avaliações</h2>
+            <ul>
+              <li>
+                <p><strong>Avaliação de João</strong></p>
+                <p>"The Big Bang Theory é uma das minhas séries favoritas. A combinação de humor e
+                  ciência é única e os personagens são inesquecíveis."</p>
+              </li>
+              <li>
+                <p><strong>Avaliação de Maria</strong></p>
+                <p>"Adoro como a série explora a dinâmica entre os personagens intelectuais e Penny. É
+                  sempre divertido assistir."</p>
+              </li>
+              <li>
+                <p><strong>Avaliação de Pedro</strong></p>
+                <p>"A série faz um ótimo trabalho ao incorporar elementos da cultura geek em seu humor.
+                  Sempre me pego rindo das piadas."</p>
+              </li>
+            </ul>
 
-                    </div>
-                </div>
-                <img src="./assets/sheldon.jpg" alt="Sheldon Cooper" />
-            </div>
+          </div>
         </div>
-    </main>
+        <img src="./assets/sheldon.jpg" alt="Sheldon Cooper" />
+      </div>
+    </div>
+  </main>
 
-    <?php include('./include/footer.php') ?>
+  <?php include('./include/footer.php') ?>
 </body>
 
 </html>
